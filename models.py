e7682bddfd4c5e010be28f6bc1dded9aab8e1402
Tracy Mazelin <tracy.mazelin@fellowshipone.com>
2022-03-28 23:25:25 -0400

    Add Employee and LeaveRequest models

--- models.py
+++ models.py
@@ -14,3 +14,28 @@ class User(UserMixin, db.Model):
 		self.name = name
 		self.email = email
 		self.password = password
+
+class Employee(UserMixin, db.Model):
+    __tablename__ = 'employee'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    manager_id = db.Column(db.Integer)
+    email = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    start_date = db.Column(db.Date, default=date.today())
+    is_admin = db.Column(db.Boolean, default=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+    user = db.relationship("User", backref="user")
+    leave = db.relationship("LeaveRequest", backref="leave", lazy="dynamic")
+
+class LeaveRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
+    leave_type_id = db.Column(db.Integer, db.ForeignKey('leave_type.id'))
+    approval_status_id = db.Column(db.Integer, db.ForeignKey('approval_status.id'))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    comment = db.Column(db.String(60))
+    created_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    employee = db.relationship("Employee", backref="employee")
